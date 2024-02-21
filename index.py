622342ef18e2e88648592b738d5183c189f0a246
Astha Choudhury <123101231+AsthaChoudhury@users.noreply.github.com>
2024-02-21 14:34:02 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, FastAPI
+from routes.performance import router as performance_entity_router
+from routes.assets import router as asset_router
+from routes.results import router as results_router
+from routes.auth import router as auth_router
+
+app = FastAPI()
+
+# defining all th3 routes
+app.include_router(asset_router, prefix='/assets', tags=['assets'])
+app.include_router(performance_entity_router,
+                   prefix='/performance', tags=['performance'])
+app.include_router(results_router, prefix='/results', tags=['results'])
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Asset Performance Analytics Dashboard!"}
