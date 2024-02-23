319a2ab2fa4865398141edfef58ce62f6df12db3
Astha Choudhury <123101231+AsthaChoudhury@users.noreply.github.com>
2024-02-23 19:56:16 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from routes.performance import router as performance_entity_router
 from routes.assets import router as asset_router
 from routes.results import router as results_router
 from routes.auth import router as auth_router
+from routes.maintenance import router as maintenance_cost_router
 
 app = FastAPI()
 
@@ -11,7 +12,9 @@ app.include_router(asset_router, prefix='/assets', tags=['assets'])
 app.include_router(performance_entity_router,
                    prefix='/performance', tags=['performance'])
 app.include_router(results_router, prefix='/results', tags=['results'])
-app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(auth_router, prefix='/auth', tags=['Authentication'])
+app.include_router(maintenance_cost_router,
+                   prefix="/maintenance", tags=["Maintenance_cost"])
 
 
 @app.get("/")
