9323fa3771371a922ddc27308ad56b9c4238a4ee
Astha Choudhury <123101231+AsthaChoudhury@users.noreply.github.com>
2024-02-23 19:54:00 +0530

    Update results.py

--- routes/results.py
+++ routes/results.py
@@ -1,20 +1,24 @@
 from fastapi import APIRouter, HTTPException
-from config.db import assets_collection, performance_metrics_collection
+from config.db import assets_collection, performance_metrics_collection, maintenance_costs_collection
 from schemas.assets import assetEntity
 
 router = APIRouter()
+
+# CALCULATION OF AVERAGE DOWNTIME
 
 
 @router.get('/average-downtime')
 async def calculate_average_downtime():
     try:
-        total_downtime = sum(asset['Downtime']
+        total_downtime = sum(asset['downtime']
                              for asset in performance_metrics_collection.find())
         total_assets = performance_metrics_collection.count_documents({})
         average_downtime = total_downtime / total_assets if total_assets != 0 else 0
         return {"average_downtime": average_downtime}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# CALCULATION OF AVERAGE UPTIME
 
 
 @router.get('/average-uptime')
@@ -28,15 +32,19 @@ async def calculate_average_uptime():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# CALCULATION OF TOTAL MAINTENANCE COSTS
+
 
 @router.get('/total-maintenance-costs')
 async def calculate_total_maintenance_costs():
     try:
         total_maintenance_costs = sum(
-            asset['maintenancecosts'] for asset in performance_metrics_collection.find())
+            asset['maintenance_cost'] for asset in maintenance_costs_collection.find())
         return {"total_maintenance_costs": total_maintenance_costs}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# CALCULATION OF ASSETS HAVING FAILURE RATE MORE THAN THRESHOLD
 
 
 @router.get('/high-failure-rate-assets')
@@ -44,7 +52,7 @@ async def find_high_failure_rate_assets(threshold: float = 0.1):
     try:
         high_failure_rate_assets = []
         for asset in performance_metrics_collection.find():
-            if asset['failurerate'] > threshold:
+            if asset['failure_rate'] > threshold:
                 asset_data = assets_collection.find_one(
                     {'_id': asset['asset_id']})
                 high_failure_rate_assets.append(assetEntity(asset_data))
